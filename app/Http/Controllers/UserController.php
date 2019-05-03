74554006eb1fead530591727cbc4b76f79127376
janaka531 <janakapradeepedirisinghe@gmail.com>
2019-05-03 23:46:05 +0530

    social network

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function postSignUp(){
+
+    }
+
+    public function postSignIn(){
+
+    }
+    
 }
+
