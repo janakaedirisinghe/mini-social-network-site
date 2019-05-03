edf21919987146c4f12d9c7a5199cb3bc3cdf429
janaka531 <janakapradeepedirisinghe@gmail.com>
2019-05-03 23:09:12 +0530

    social network

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,20 +7,38 @@
 @section('content')
 	
 	<div class="row">
-		<div class="col-6">
+		<div class="col-md-6">
+			<h3>Sign Up</h3>
 			<form method="post" action="#">
 				<div class="form-group">
 					<label for="email"> Email</label>
 					<input type="text" class="form-control" name="email" id="email">
 				</div>
 				<div class="form-group">
-					<label for="email"> Email</label>
-					<input type="text" class="form-control" name="email" id="email">
+					<label for="first_name"> First Name</label>
+					<input type="text" class="form-control" name="first_name" id="first_name">
 				</div>
+				<div class="form-group">
+					<label for="password"> Password</label>
+					<input type="password" class="form-control" name="password" id="password">
+				</div>
+				<button type="submit" class="btn btn-primary"> Submit</button>
+			</form>
+		</div>
+
+		<div class="col-md-6">
+			<h3>Sign In</h3>
+			<form method="post" action="#">
 				<div class="form-group">
 					<label for="email"> Email</label>
 					<input type="text" class="form-control" name="email" id="email">
 				</div>
+				
+				<div class="form-group">
+					<label for="password"> Password</label>
+					<input type="password" class="form-control" name="password" id="password">
+				</div>
+				<button type="submit" class="btn btn-primary"> Submit</button>
 			</form>
 		</div>
 	</div>		
