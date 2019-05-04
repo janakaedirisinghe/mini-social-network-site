41d16a0cac4f306968476b5ef927bf507898a471
janaka531 <janakapradeepedirisinghe@gmail.com>
2019-05-04 12:26:59 +0530

    new feature

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -34,4 +34,12 @@ class PostController extends Controller
     	return redirect() -> route('dashboard')->with(['message' => $message])  ;
 
     }
+
+    public function getDeletePost($post_id){
+    		$post = Post::where('id',$post_id)->first();
+    		$post->delete();
+    		return redirect() -> route('dashboard')->with(['message' => 'Successfully Deleted!']);
+
+
+    }
 }
