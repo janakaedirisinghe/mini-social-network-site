b405ef25bb174aa2459946eef8bfdd9788ac5363
janaka531 <janakapradeepedirisinghe@gmail.com>
2019-05-04 19:27:10 +0530

    like n dislike

---
+++ app/Like.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Like extends Model
+{
+    public function user(){
+    	return $this->belongsTo('App\User');
+    }
+
+    public function post(){
+    	return $this->belongsTo('App\Post');
+    }
+}
