3854229816ebc4a81e9eaea8c28d8777909bba05
janaka531 <janakapradeepedirisinghe@gmail.com>
2019-05-04 15:59:39 +0530

    bug fix

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,21 +1,23 @@
+var postId = 0;
+var postBodyElement = null;
+
 $('.posts').find('.post').find('.interaction').find('.badge-success').on('click',function(event){
 	//console.log('it works');
 event.preventDefault();
-var postBody = event.target.parentNode.parentNode.childNodes[1].textContent;
-//console.log(postBody);
 
-$('#post-body').val(postBody);
-
-	$('#edit-modal').modal();
+    postBodyElement = event.target.parentNode.parentNode.childNodes[1];
+    var postBody = postBodyElement.textContent;
+    postId = event.target.parentNode.parentNode.dataset['postid'];
+    $('#post-body').val(postBody);
+    $('#edit-modal').modal();
 });
 
 $('#model-save').on('click',function(){
-	$.ajax()({
-		method: 'POST',
-		url: url,
-		data: { body: $('#post-body').val(), postId: '', _token: token }
-
-	})
+	 $.ajax({
+            method: 'POST',
+            url: url,
+            data: {body: $('#post-body').val(), postId: postId, _token: token}
+        })
 		.done(function(msg){
 			console.log(msg['message']); 
 		});
