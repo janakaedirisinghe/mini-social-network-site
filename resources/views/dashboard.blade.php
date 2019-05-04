d8334916c14c3877d9ef8e124826b5ea8e804de4
janaka531 <janakapradeepedirisinghe@gmail.com>
2019-05-04 21:18:25 +0530

    update like n dislike feature

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -34,8 +34,9 @@
 							Post by {{$post->user->first_name}} on {{ $post-> created_at}}
 						</div>
 						<div class="interaction">
-							<a href="#" class="like">Like</a> 
-							<a href="#" class="like">Dislike</a> 
+
+							<a href="#" class="like">{{ Auth::user()->likes()->where('post_id',$post->id)->first() ? Auth::user()->likes()->where('post_id',$post->id)->first()->like == 1 ? 'You like this post' : 'Like' : 'Like'}}</a> 
+							<a href="#" class="like">{{ Auth::user()->likes()->where('post_id',$post->id)->first() ? Auth::user()->likes()->where('post_id',$post->id)->first()->like == 0 ? 'You don\'t like this post' : 'Dislike' : 'Dislike'}}</a> 
 
 								@if(Auth::user() == $post->user)
 										<a href="#" class="badge badge-pill badge-success">Edit</a> 
